ecec1c06860e4a5138d8c4f5292cdb38adaa9733
slotherinee <testfordev121@gmail.com>
2024-08-14 21:54:31 +0300

    Fixed ts types

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -1,4 +1,4 @@
-const Skeleton = ({ className }) => (
+const Skeleton = ({ className }: { className: string }) => (
   <div aria-live="polite" aria-busy="true" className={className}>
     <span className="inline-flex w-full animate-pulse select-none rounded-md bg-gray-300 leading-none">
       ‌
@@ -7,7 +7,7 @@ const Skeleton = ({ className }) => (
   </div>
 );
 
-const SVGSkeleton = ({ className }) => (
+const SVGSkeleton = ({ className }: { className: string }) => (
   <svg className={className + " animate-pulse rounded bg-gray-300"} />
 );
 
